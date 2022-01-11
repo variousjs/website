e86ccd963ae6ebb6af317e0fb7b77994c9b3be7c
loeify <loeify@gmail.com>
2022-01-11 08:57:38 +0800

    feat: nav link

---
+++ src/components/nav-link/index.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from 'react'
+import { ComponentProps, Link } from '@variousjs/various'
+import { Config } from '../../types'
+import csses from './index.less'
+
+const N: FC<ComponentProps<{}, Config>> = ({ $config, $router }) => {
+  const navs = $config.nav
+  const path = $router!.location.pathname
+
+  const index = navs.findIndex((item) => item.path === path)
+  const nav = {
+    prev: navs[index - 1],
+    next: navs[index + 1],
+  }
+
+  return (
+    <div className={csses.links}>
+      {
+        nav.prev ? (
+          <div>
+            <p>上一篇</p>
+            <Link to={nav.prev.path}>{nav.prev.name}</Link>
+          </div>
+        ) : <span />
+      }
+      {
+        nav.next ? (
+          <div>
+            <p>下一篇</p>
+            <Link to={nav.next.path}>{nav.next.name}</Link>
+          </div>
+        ) : <span />
+      }
+    </div>
+  )
+}
+
+export default N
--- src/entry/container.tsx
+++ src/entry/container.tsx
@@ -1,17 +1,40 @@
-import React, { Component, ComponentType } from 'react'
+import React, { Component, ComponentType, createRef } from 'react'
 import { ContainerProps, Route, Router } from '@variousjs/various'
 import { Store, Config } from '../types'
 import csses from './entry.less'
 
 class Container extends Component<ContainerProps<Store, Config>> {
+  leftRef = createRef<HTMLDivElement>()
+
+  unListen: () => void
+
   componentDidMount() {
     document.documentElement.classList.remove('loading')
+    this.unListen = this.props.$router.history.listen(() => {
+      this.leftRef.current?.scrollTo(0, 0)
+    })
+  }
+
+  componentWillUnmount() {
+    this.unListen && this.unListen()
+  }
+
+  onMDClick = (e: React.MouseEvent<HTMLElement>) => {
+    const target = e.target as Element
+    if (target.tagName === 'A') {
+      const href = target.getAttribute('href')
+      if (href?.slice(0, 1) !== '#') {
+        e.preventDefault()
+        window.open(href as string)
+      }
+    }
   }
 
   render() {
     const { $component, $config } = this.props
     const Header = $component('header')
     const Nav = $component('nav')
+    const NavLink = $component('nav-link')
 
     return (
       <div className={csses.container}>
@@ -19,8 +42,11 @@ class Container extends Component<ContainerProps<Store, Config>> {
           <Header />
         </div>
 
-        <div className={csses.content}>
-          <div className={csses.left}>
+        <div
+          className={csses.content}
+          onClick={this.onMDClick}
+        >
+          <div ref={this.leftRef} className={csses.left}>
             <Router>
               {
                 $config.pages.map(({ path, components }) => {
@@ -40,6 +66,7 @@ class Container extends Component<ContainerProps<Store, Config>> {
                 })
               }
             </Router>
+            <NavLink />
           </div>
 
           <div className={csses.right}>
